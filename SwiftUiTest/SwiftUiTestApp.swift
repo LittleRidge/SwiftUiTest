722a71817ad6109d4015df8684bf4ccd4cc592d8
Евгений Сергеев <e.sergeev@mintmail.ru>
2024-09-26 11:24:58 +0400

    Initial Commit

---
+++ SwiftUiTest/SwiftUiTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiTestApp.swift
+//  SwiftUiTest
+//
+//  Created by Евгений Сергеев on 26.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUiTestTests/SwiftUiTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiTestTests.swift
+//  SwiftUiTestTests
+//
+//  Created by Евгений Сергеев on 26.09.2024.
+//
+
+import Testing
+@testable import SwiftUiTest
+
+struct SwiftUiTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
